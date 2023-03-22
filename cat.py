3732aaed88e353aeb57246be190fd2eeca2af27c
Mr. Park <72147590+mrparkyrdsb@users.noreply.github.com>
2023-03-22 17:58:57 +0000

    completed cat.py and main.py answer key

--- cat.py
+++ cat.py
@@ -1,1 +1,45 @@
 # cat.py
+# external imports
+from random import choice
+
+class ScratchPost:
+    def __init__(self):
+        self.hp = 10
+
+    def use(self):
+        if self.hp > 0:
+            self.hp -= 1
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return f'A scratch post. Scratches left: {self.hp}.'
+
+    def __repr__(self):
+        return self.__str__()
+
+class Cat:
+    def __init__(self, name, breed, colour):
+        self.name = name
+        self.breed = breed
+        self.colour = colour
+        self.is_hungry = choice([True, False])
+        self.wants_to_scratch = choice([True, False])
+
+    def eat(self, food):
+        if self.is_hungry:
+            print(f'{self.name} is eating {food}.')
+            self.is_hungry = False
+        else:
+            print(f'{self.name} is not hungry.')
+
+    def scratch(self, post):
+        if not self.wants_to_scratch:
+            print(f'{self.name} does not need to scratch.')
+        else:
+            if post.use():
+                self.wants_to_scratch = False
+                print(f'{self.name} scratched the post.')
+            else:
+                print(f'{self.name} destroyed this post and cannot scratch it anymore.')
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # Main.py
+from cat import Cat
+from cat import ScratchPost
+
+post = ScratchPost()
+cat1 = Cat('Cheetos', 'Tabby', 'Orange')
+
+# cat data
+print(cat1)
+
+# post data
+print(post)
+
+# is cat1 hungry?
+print(cat1.is_hungry)
+cat1.eat('tuna')
+
+# does cat1 need to scratch?
+print(cat1.wants_to_scratch)
+cat1.scratch(post)
+
+print(post)
